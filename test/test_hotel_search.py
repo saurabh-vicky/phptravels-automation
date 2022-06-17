32a56248cfa5b8e51150995c78be81a176ef7bc4
saurabh-vicky <47377454+saurabh-vicky@users.noreply.github.com>
2022-06-17 11:25:48 +0530

    Update test_hotel_search.py

--- test/test_hotel_search.py
+++ test/test_hotel_search.py
@@ -5,8 +5,8 @@ from src.page_object.locators import SearchResultsLocators
 def test_hotel_search(web_driver):
     search_hotel = SearchHotelForm(web_driver)
     search_hotel.open_page()
-    search_hotel.set_destination("Warsaw")
-    search_hotel.set_date_range("29/12/2019", "03/01/2020")
+    search_hotel.set_destination("London")
+    search_hotel.set_date_range("16/06/2022", "24/06/2022")
     search_hotel.set_adults_number(3)
     search_hotel.set_kids_number(0)
     search_hotel.search_perform()
